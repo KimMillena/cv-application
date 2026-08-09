26730f7fc342d514f58ba8165abbe70b4df9c880
Kim <kimmillena09@gmail.com>
2026-08-10 02:07:48 +0800

    Display education details to resume

--- src/App.jsx
+++ src/App.jsx
@@ -80,6 +80,7 @@ function App() {
       ))}
       <ResumeSection 
         personalDetails={personalDetails}
+        educationList={educationList}
       />
     </>
   )
--- src/components/resume-section/ResumeSection.jsx
+++ src/components/resume-section/ResumeSection.jsx
@@ -1,18 +1,22 @@
-function ResumeSection({ personalDetails }) {
+function ResumeSection({ personalDetails, educationList }) {
     return (
-        <>
+        <div className="resume-section">
             <h1>Resume</h1>
             <p>{personalDetails.firstName}</p>
             <p>{personalDetails.lastName}</p>
             <p>{personalDetails.email}</p>
             <p>{personalDetails.phoneNumber}</p>
             <p>{personalDetails.address}</p>
-{/* 
-            <p>{educationDetails.school}</p>
-            <p>{educationDetails.degree}</p>
-            <p>{educationDetails.startDate}</p>
-            <p>{educationDetails.endDate}</p> */}
-        </>
+
+            {educationList.map(education => (
+                <div className="education-info-section">
+                    <p>{education.school}</p>
+                    <p>{education.degree}</p>
+                    <p>{education.startDate}</p>
+                    <p>{education.endDate}</p>
+                </div>
+            ))}
+        </div>
     );
 }
 
